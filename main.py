534a51d3b8ca17e382bf9467c06d327a7bd2eb65
vishalgddam873 <vishal18@navgurukul.org>
2023-05-17 15:17:31 +0530

    Initial comment

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# Importing Library
 import cv2
 
-# Define the input path
 inputPath = 'static/img1.jpg'
 
-# Load the color image
 orignalImage = cv2.imread(inputPath)
 
 # ------------Convert image to Grayscale --------------
 
 # Convert the color image to grayscale image
-grayscaleImage = cv2.cvtColor(orignalImage, cv2.COLOR_BGR2GRAY)
+
 
 # # ----------------- Convert image to Sketch Image ---------------
 
@@ -24,10 +21,10 @@ grayscaleImage = cv2.cvtColor(orignalImage, cv2.COLOR_BGR2GRAY)
 
 
 # Save the sketch image to disk
-outputPath = 'converted/sketch.png'
+
 
 # Display the converted image
 
+cv2.waitKey(0)
 
 # Display a message indicating that the image has been saved
-print('Converted Sketch image saved to disk: ' + outputPath)
